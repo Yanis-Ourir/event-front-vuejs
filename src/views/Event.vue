8626f068813ad96788eaadf61c363a383c046b81
Yanis <ourir.yanis@gmail.com>
2025-03-11 23:39:28 +0100

    test

--- src/views/Event.vue
+++ src/views/Event.vue
@@ -4,6 +4,17 @@
 
 <template>
     <h1>
-        Event
+        Event PAGE
     </h1>
-</template>
+
+    <!-- Event Name -->
+    
+    <label for="event-name" class="text-gray-300 mb-2">Event Name</label>
+    <div class="relative">
+        <input type="text" id="event-name" placeholder="Event Name"
+            class="bg-gray-800 w-full rounded-lg px-4 py-2 border border-gray-700 focus:ring-2 focus:ring-violet-500 focus:outline-none transition-all" />
+        <span class="absolute right-3 top-3 text-gray-400">
+            📧
+        </span>
+    </div>
+    </template>
