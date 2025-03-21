4764461a53c22fea41aa14edb558ca810745708f
Yanis <ourir.yanis@gmail.com>
2025-03-21 20:11:51 +0100

    added function for api

--- src/functions/event.ts
+++ src/functions/event.ts
@@ -7,16 +7,58 @@ type Event = {
 
 export function createEvent(data: Event) {
     console.log('Event created', data);
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization': 'Bearer' + data // Add token here,
+        },
+    })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+    console.log('Event created', data);
 }
 
 export function updateEvent(data: Event) {
     console.log('Event updated', data);
+    fetch('https://jsonplaceholder.typicode.com/posts/1', {
+        method: 'PUT',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization': 'Bearer' + data // Add token here,
+        },
+    })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+    
 }
 
 export function deleteEvent(data: Event) {
     console.log('Event deleted', data);
+    fetch('https://jsonplaceholder.typicode.com/posts/1', {
+        method: 'DELETE',
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization': 'Bearer' + data // Add token here,
+        },
+    })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+    console.log('Event deleted', data);
 }
 
 export function getEvents(data: Event) {
+    console.log('Event fetched', data);
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'GET',
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization': 'Bearer' + data // Add token here,
+        },
+    })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
     console.log('Event fetched', data);
 }
