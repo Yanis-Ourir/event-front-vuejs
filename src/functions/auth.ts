9d3e2b9083e1ea4ad8a13e2120ac1ad39b1f93e6
Yanis-Ourir <ourir.yanis@gmail.com>
2025-03-25 21:01:17 +0100

    token function added

--- src/functions/auth.ts
+++ src/functions/auth.ts
@@ -4,6 +4,26 @@ export type User = {
 }
 
 export function logUserIn(data: User) {
-    console.log('User logged in', data);
+    fetch('http://localhost:3000/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+        localStorage.setItem('token', data.token);
+    }
+    )
+    .catch((error) => {
+        console.error('Error:', error);
+    }
+    );
+}
 
+export function logout() {
+    // remove token from local storage
+    localStorage.removeItem('token');
 }
